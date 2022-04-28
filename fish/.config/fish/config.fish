690f73f6b70acbffbbc782bcf6824349e5398d38
Alexandre Leroux <8794280+nleof@users.noreply.github.com>
2022-04-28 21:58:49 +0200

    ref(fish): remove greetings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,5 +7,7 @@ alias vim="nvim"
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 
+set fish_greeting
+
 direnv hook fish | source
 starship init fish | source
