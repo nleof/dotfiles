dca41f1a361ea36508b7b817f6dcbdb259019407
Alexandre Leroux <8794280+nleof@users.noreply.github.com>
2022-05-11 22:31:47 +0200

    feat(fish): add ls alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias l="ls -alh"
+alias cat="bat --theme='Solarized (light)'"
 alias vi="nvim"
 alias vim="nvim"
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
