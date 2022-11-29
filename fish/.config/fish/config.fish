d7ec68984fabd23626e43c09e0905a76cdae970f
Alexandre Leroux <alexandre@yawp.fr>
2022-11-29 21:11:33 +0100

    feat(fish): enable vi keys

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,6 +10,15 @@ alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.ar
 
 set fish_greeting
 
+function fish_hybrid_key_bindings --description \
+"Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings fish_hybrid_key_bindings
+
 set -x DIRENV_LOG_FORMAT ""
 direnv hook fish | source
 starship init fish | source
