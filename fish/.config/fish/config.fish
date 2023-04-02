1bd315243f0305dda3ce6f47b2273d6cfef354cc
Alexandre Leroux <8794280+nleof@users.noreply.github.com>
2023-04-02 10:28:41 +0200

    feat(fish): add vimdiff alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ end
 alias l="ls -alh"
 alias vi="nvim"
 alias vim="nvim"
+alias vimdiff="nvim -d"
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias cat="bat"
