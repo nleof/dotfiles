7ab6e6569d89ded29666739367fdc400444e44f7
Alexandre Leroux <8794280+nleof@users.noreply.github.com>
2023-04-06 20:29:50 +0200

    ref(emacs): remove magit-delta

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -58,9 +58,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(use-package magit-delta
-  :ensure t
-  :hook (magit-mode . magit-delta-mode))
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
 (setq spell-dictionary "en_US")
